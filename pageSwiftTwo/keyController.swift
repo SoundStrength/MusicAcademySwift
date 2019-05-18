44b43a36e0cd1caa04945bc8b9500289b6933803
Aidan <drwhovian@gmail.com>
2019-05-18 15:48:47 +1200

    All keys now have notes. Range is c4-d5

--- pageSwiftTwo/keyController.swift
+++ pageSwiftTwo/keyController.swift
@@ -11,13 +11,38 @@ class keyController: UIViewController, AVAudioPlayerDelegate {
     
     var chordPlayer:AVAudioPlayer!
     var c4player:AVAudioPlayer!
+    var cs4player:AVAudioPlayer!
     var d4player:AVAudioPlayer!
+    var ds4player:AVAudioPlayer!
     var e4player:AVAudioPlayer!
+    var f4player:AVAudioPlayer!
+    var fs4player:AVAudioPlayer!
+    var g4player:AVAudioPlayer!
+    var gs4player:AVAudioPlayer!
+    var a4player:AVAudioPlayer!
+    var as4player:AVAudioPlayer!
+    var b4player:AVAudioPlayer!
+    var c5player:AVAudioPlayer!
+    var cs5player:AVAudioPlayer!
+    var d5player:AVAudioPlayer!
+    
     
     let chordURL = Bundle.main.url(forResource: "chordSeq1", withExtension: "wav")
     let c4URL = Bundle.main.url(forResource: "c4", withExtension: "mp3")
+    let cs4URL = Bundle.main.url(forResource: "c#4", withExtension: "mp3")
     let d4URL = Bundle.main.url(forResource: "d4", withExtension: "mp3")
+    let ds4URL = Bundle.main.url(forResource: "d#4", withExtension: "mp3")
     let e4URL = Bundle.main.url(forResource: "e4", withExtension: "mp3")
+    let f4URL = Bundle.main.url(forResource: "f4", withExtension: "mp3")
+    let fs4URL = Bundle.main.url(forResource: "f#4", withExtension: "mp3")
+    let g4URL = Bundle.main.url(forResource: "g4", withExtension: "mp3")
+    let gs4URL = Bundle.main.url(forResource: "g#4", withExtension: "mp3")
+    let a4URL = Bundle.main.url(forResource: "a4", withExtension: "mp3")
+    let as4URL = Bundle.main.url(forResource: "a#4", withExtension: "mp3")
+    let b4URL = Bundle.main.url(forResource: "b4", withExtension: "mp3")
+    let c5URL = Bundle.main.url(forResource: "c5", withExtension: "mp3")
+    let cs5URL = Bundle.main.url(forResource: "c#5", withExtension: "mp3")
+    let d5URL = Bundle.main.url(forResource: "d5", withExtension: "mp3")
     
     var playing = false //Boolean used to keep track of whether the chord sequence is currently playing or not.
     
@@ -49,6 +74,7 @@ class keyController: UIViewController, AVAudioPlayerDelegate {
         }
     }
     
+    
     @IBAction func c4(_ sender: UIButton) {
         do {
             c4player = try AVAudioPlayer(contentsOf: c4URL!)
@@ -56,13 +82,27 @@ class keyController: UIViewController, AVAudioPlayerDelegate {
         catch {
             print(error)
         }
-        
+        c4player.setVolume(0.25, fadeDuration: 0.0)
         c4player.stop()
         c4player.play()
     }
     
     @IBAction func cs4(_ sender: UIButton) {
+        do {
+            cs4player = try AVAudioPlayer(contentsOf: cs4URL!)
+        }
+        catch {
+            print(error)
+        }
+        cs4player.setVolume(0.25, fadeDuration: 0.0)
+        cs4player.stop()
+        cs4player.play()
+    
     }
+    
+    
+    
+    
     
     @IBAction func d4(_ sender: UIButton) {
         do {
@@ -71,12 +111,21 @@ class keyController: UIViewController, AVAudioPlayerDelegate {
         catch {
             print(error)
         }
-        
+        d4player.setVolume(0.25, fadeDuration: 0.0)
         d4player.stop()
         d4player.play()
     }
     
     @IBAction func ds4(_ sender: UIButton) {
+        do {
+            ds4player = try AVAudioPlayer(contentsOf: ds4URL!)
+        }
+        catch {
+            print(error)
+        }
+        ds4player.setVolume(0.25, fadeDuration: 0.0)
+        ds4player.stop()
+        ds4player.play()
     }
     
     @IBAction func e4(_ sender: UIButton) {
@@ -86,11 +135,130 @@ class keyController: UIViewController, AVAudioPlayerDelegate {
         catch {
             print(error)
         }
-        
+        e4player.setVolume(0.25, fadeDuration: 0.0)
         e4player.stop()
         e4player.play()
     }
     
+    @IBAction func f4(_ sender: UIButton) {
+        do {
+            f4player = try AVAudioPlayer(contentsOf: f4URL!)
+        }
+        catch {
+            print(error)
+        }
+        f4player.setVolume(0.25, fadeDuration: 0.0)
+        f4player.stop()
+        f4player.play()
+    }
+    
+    @IBAction func fs4(_ sender: UIButton) {
+        do {
+            fs4player = try AVAudioPlayer(contentsOf: fs4URL!)
+        }
+        catch {
+            print(error)
+        }
+        fs4player.setVolume(0.25, fadeDuration: 0.0)
+        fs4player.stop()
+        fs4player.play()
+    }
+    
+    @IBAction func g4(_ sender: UIButton) {
+        do {
+            g4player = try AVAudioPlayer(contentsOf: g4URL!)
+        }
+        catch {
+            print(error)
+        }
+        g4player.setVolume(0.25, fadeDuration: 0.0)
+        g4player.stop()
+        g4player.play()
+    }
+    
+    @IBAction func gs4(_ sender: UIButton) {
+        do {
+            gs4player = try AVAudioPlayer(contentsOf: gs4URL!)
+        }
+        catch {
+            print(error)
+        }
+        gs4player.setVolume(0.25, fadeDuration: 0.0)
+        gs4player.stop()
+        gs4player.play()
+    }
+    
+    @IBAction func a4(_ sender: UIButton) {
+        do {
+            a4player = try AVAudioPlayer(contentsOf: a4URL!)
+        }
+        catch {
+            print(error)
+        }
+        a4player.setVolume(0.25, fadeDuration: 0.0)
+        a4player.stop()
+        a4player.play()
+    }
+    
+    @IBAction func as4(_ sender: UIButton) {
+        do {
+            as4player = try AVAudioPlayer(contentsOf: as4URL!)
+        }
+        catch {
+            print(error)
+        }
+        as4player.setVolume(0.25, fadeDuration: 0.0)
+        as4player.stop()
+        as4player.play()
+    }
+    
+    @IBAction func b4(_ sender: UIButton) {
+        do {
+            b4player = try AVAudioPlayer(contentsOf: b4URL!)
+        }
+        catch {
+            print(error)
+        }
+        b4player.setVolume(0.25, fadeDuration: 0.0)
+        b4player.stop()
+        b4player.play()
+    }
+    
+    @IBAction func c5(_ sender: UIButton) {
+        do {
+            c5player = try AVAudioPlayer(contentsOf: c5URL!)
+        }
+        catch {
+            print(error)
+        }
+        c5player.setVolume(0.25, fadeDuration: 0.0)
+        c5player.stop()
+        c5player.play()
+    }
+    
+    @IBAction func cs5(_ sender: UIButton) {
+        do {
+            cs5player = try AVAudioPlayer(contentsOf: cs5URL!)
+        }
+        catch {
+            print(error)
+        }
+        cs5player.setVolume(0.25, fadeDuration: 0.0)
+        cs5player.stop()
+        cs5player.play()
+    }
+    
+    @IBAction func d5(_ sender: UIButton) {
+        do {
+            d5player = try AVAudioPlayer(contentsOf: d5URL!)
+        }
+        catch {
+            print(error)
+        }
+        d5player.setVolume(0.25, fadeDuration: 0.0)
+        d5player.stop()
+        d5player.play()
+    }
     
     /*
     // MARK: - Navigation
